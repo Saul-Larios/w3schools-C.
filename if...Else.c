a7aeed454af73250f7ad8eea3325d023b593f2db
Jesus Larios <sau30jas@gmail.com>
2022-05-09 22:48:48 -0500

    Agregando if ...Else ejemplos

---
+++ if...Else.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(){
+    // In the example below, we test two values to find out if 20 is greater than 18. If the condition is true, print some text
+    if(20 > 18) {
+        printf("20 is greater than 18\n");
+    }
+    
+    // We can also test variables
+    int x = 20;
+    int y = 18;
+    if (x > y) {
+        printf("x is greater than y\n");
+    }
+    
+    // Use the else statement to specify a block of code to be executed if the condition is false
+    int time = 20;
+    if (time > 18) {
+        printf("Good day.\n");
+    } else {
+        printf("Good evening.\n");
+    }
+    
+    // Use the else if statement to specify a new condition if the first condition is false.
+    time = 22;
+    if (time < 10) {
+        printf("Good morning.\n");
+    } else if (time < 20) {
+        printf("Good day.\n");
+    } else {
+        printf("Good evening.\n");
+    }
+    // Outputs "Good evening"
+
+    //This example shows how you can use if..else if to find out if a number is positive or negative
+    int myNum = 10;
+
+    if(myNum > 0)
+    printf("the value is a positive number.\n");
+    else if (myNum < 0)
+    printf("The value is a negative number.");
+    else
+    printf("The value is 0.");
+    return 0;
+}
